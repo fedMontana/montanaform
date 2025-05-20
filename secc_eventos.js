bb2e8dac45d861c2b5bc165854b9540e97bc4271
fedMontana <fmdmye@gmail.com>
2025-05-20 09:16:35 -0600

    secc_eventos.js

--- secc_eventos.js
+++ secc_eventos.js
@@ -1,8 +1,8 @@
 "use strict";
 
 
-let nombreOriginal = "";  // Nombre original del evento o competencia
-let modoEdicion = false;  // Modo edición del evento o competencia
+let nombreOriginal = "";
+let modoEdicion = false;
 let opcEventos = [];
 
 const beliminarEvento = $("eliminarEvento");
@@ -11,10 +11,6 @@ const bsalvarEvento = $("salvarEvento");
 const eventosSelect = $("eventosSelect");
 const detallesEventos = $("eventosDetails");
 
-//const contenedor = $('previewCompEvento');
-
-/* 
-*/
 function inicializarEventos() {
   cargaDatosSelectEV(eventos);  
   document.getElementById("fotoEventoInput").value = "";
@@ -69,8 +65,7 @@ function inicializarEventos() {
 
       let destino = "NuevoEvento"; // presuponemos
       if (modoEdicion) {
-        // Estamos en modo edición, en este modo se puede enviar o no una nueva imagen
-        // Es necesario enviar el nombre original del evento
+        
         destino = "EditarEvento";
         datos["nombreOriginal"] = nombreOriginal;
       }
@@ -106,14 +101,7 @@ function inicializarEventos() {
 
   document.querySelectorAll('.edit-field-evento').forEach(field => {
     field.addEventListener('input', () => {
-      // Obtenemos la ID del control para poder configurar el nombre del valor en el JSON
-      //let nmb = field.id.toString().replace("Input", "");
-      //nmb = nmb[0].toUpperCase() + nmb.slice(1);
-      //console.log("El control del cambio fue: ", field.name);
-      // Se comparan valores, 
-      // El valor constante para identificar dónde está ocurriendo la modificación es por
-      // medio del comboBox de selección de eventos, ya que tiene la opción de "Nuevo evento"
-      // y los eventos descargados.
+      
       console.log("Selección hecha por el usuario: ", eventosSelect.value);
       if (eventosSelect.value === "Nuevo evento") {
         if (field.value === "") {
@@ -126,25 +114,11 @@ function inicializarEventos() {
       } else {
         // Comparamos los valores ingresados        
       }
-      /*if (initialData[nmb] !== field.value) {
-        // El usuario modificó un campo, se resalta cambiando el color del borde.
-        field.style.borderColor = "#ff9800";
-        contCambios++;
-        if (contCambios === 1) {
-          // Ocurrió la primera modificación, cuando esto sucede se deshabilitan
-          // los botones...
-        }
-      } else {
-        // El usuario regresó al valor previo del cambio en el campo
-        field.style.borderColor = "#ccc";
-        contCambios--;
-      }*/
+      
     });
   });
 }
 
-// Cuando el usuario modificó la información del evento se actualizará
-// el objeto "eventos" que es donde está almacenada a información.
 function modificarArr(nuevaImg) {
   // Buscar el objeto evento en el array
   const evento = eventos.find(e => e["Nombre del evento"] === nombreOriginal);
@@ -156,8 +130,7 @@ function modificarArr(nuevaImg) {
           evento["Imagen"] = nuevaImg;
       } else {
         const viewField = $(fieldId); // fieldId corresponde al nombre de los controles html
-        //console.log("Valor nuevo: ", viewField.value, "  Valor actual: ", evento[config]);
-        // Actualizamos el campo en el objeto
+        
         evento[config] = viewField.value;
       }
     });
@@ -202,14 +175,11 @@ function habilitaBotonesPArteEvento() {
   eventosSelect.dispatchEvent(new Event('change'));
 }
 
-function populateFormEventos(selectedEvento) {
-  //console.log("Examinando estructura evento: ", selectedEvento);  
+function populateFormEventos(selectedEvento) {  
   Object.entries(fieldMappingEventos).forEach(([fieldId, config]) => {
-    const viewField = $(fieldId); // fieldId corresponde al nombre de los controles html
-    //console.log("fieldId: ", fieldId, "   config: ", config);
+    const viewField = $(fieldId); // fieldId corresponde al nombre de los controles html    
     switch (fieldId) {
-      case "eventPoster":
-        //console.log("Sí, acá va el poster: ", selectedEvento[config]);
+      case "eventPoster":        
         imagenContenedor('previewCompEvento', "fotoEventoInput", selectedEvento[config]);
         break;
       case "eventDuration":
@@ -222,8 +192,6 @@ function populateFormEventos(selectedEvento) {
   });
 }
 
-/* Esta función compartida permite visualizar una imagen con iframe tomando la url proporcionada por GAS  
-*/
 function imagenContenedor(elContenedor, elInput, urlImg) {
   const contenedor = $(elContenedor);
   const laInput = $(elInput);
@@ -252,82 +220,6 @@ function imagenContenedor(elContenedor, elInput, urlImg) {
   base64Comprobante = null;
   imagenProcesadaOK = false;
 }
-
-/* Función compartida
-*/
-function creaObjetoVacio(fieldMapping) {
-  let objVacio = {};
-  Object.entries(fieldMapping).forEach(([fieldId, config]) => {
-    objVacio[config] = "";
-  });
-  return objVacio;
-}
-
-
-/* ¿Qué hace esta función
-  Recibe como parámetro un fieldMapping el cual contiene la relación entre los nombres
-  de los controles html con el valor correspondiente. Por ejemplo para eventos y competencias:
-  {'eventName': 'Nombre del evento', }   {"competenciaName": "Nombre competencia",}
-?*/
-function camposHtmlAObjeto(fieldMapping) {
-  const datos = {};
-  // Recorremos el fieldMappingEventos.
-  // idCampo es el nombre del control html  ||| nombreHeader es el valor del campo recibido en el JSON
-  Object.entries(fieldMapping).forEach(([idCampo, nombreHeader]) => {
-    //            condición ? expresiónSiVerdadero : expresiónSiFalso
-    const valor = document.getElementById(idCampo)?.value || "";
-    datos[nombreHeader] = valor;
-  });
-  // Campos extra fuera del mapping
-  datos["Usuario Organizador"] = elCorreo;
-  datos["Asociación Organizador"] = nombreAsociacion;
-
-  return datos;
-}
-
-function eliminarEventoCompe(arrayTarget) {
-  const keys = Object.keys(arrayTarget[0]);
-  // let claveNombre = keys.includes("Nombre del evento") ? "Nombre del evento" : "Nombre competencia";
-  let claveNombre = null;
-
-  if (keys.includes("Nombre del evento")) {
-    claveNombre = "Nombre del evento";
-  } else if (keys.includes("Nombre competencia")) {
-    claveNombre = "Nombre competencia";
-  } else {
-    console.log("No se encontró una clave válida");
-    return;
-  }
-
-  // Con .filter() se recorre todo el array
-  const arrayActualizado = arrayTarget.filter(loBuscado => loBuscado[claveNombre] !== nombreOriginal);
-  arrayTarget.length = 0;  // Se vacía el array original
-  arrayTarget.push(...arrayActualizado); // Se rellena con el nuevo contenido
-  console.log("Se ha eliminado el array satisfactoriamente.");
-}
-
-/* Recibe como parámetro la referencia del array (puede ser eventos o competencias)
-  Para generar las opciones en el select donde el usuario selecciona un evneto
-  o competencia para visualizar o editar
-*/
-function cargaDatosSelectEV(arrayTarget) {
-  let valSelect = [];
-  const keys = Object.keys(arrayTarget[0]);
-  let claveNombre = "Nombre competencia";
-  valSelect[0] = "Nueva competencia"; // presuponemos
-  let idControl = "competenciasSelect";
-  if (keys.includes("Nombre del evento")) {
-    claveNombre = "Nombre del evento";
-    valSelect[0] = "Nuevo evento";
-    idControl = "eventosSelect";
-  }
-  // Empieza el relleno del <select>
-  arrayTarget.forEach(nombre => {
-    valSelect.push(nombre[claveNombre]);
-  });
-  generarOpcionesSelect(idControl, valSelect);
-}
-
 
 function revisaModificaciones(fieldMapping) {
   //objCambios
