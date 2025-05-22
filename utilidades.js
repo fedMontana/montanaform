9c6bd0ac3d66648a9783e0555b275090ae4190d9
fedMontana <fmdmye@gmail.com>
2025-05-22 16:58:30 -0600

    utilidades.js

--- utilidades.js
+++ utilidades.js
@@ -1,6 +1,6 @@
 const URL_ACTIVA = "https://script.google.com/macros/s/AKfycbxDk_jj36WwMxH1wKp21PBszzi2vNgRYozLFet_nNdwiheEN6sVp8w1c_0ENatNRqt0PQ/exec"; //accesoVer09b
 
-const URL_ACTIVA0 = 'https://script.google.com/macros/s/AKfycbzwdRMerXnK-6tRVAgh5wOz2Ah361rnamVMxx4YxJrRYS-zT34IxKWFhX-VZSzCpcfbWA/exec';
+const URL_ACTIVA0 = 'https://script.google.com/macros/s/AKfycbyJNeJQXPIqHN5KPR4fFemte7A0DWNebCvtKuFd1OLQexoZTYuk69lL4pU-omY90yj_Wg/exec'; // form01c
 
 //
 function $(id) {
